59f64c4ca3b16061d17c82f7489aabc41c990e87
ScandicCon <glhgygi.yiug@gmail.com>
2026-04-28 00:24:12 +0300

    docker + frontend

--- app/db/session.py
+++ app/db/session.py
@@ -1,19 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from app.core.config import DATABASE_URL
 
-DATABASE_URL = "sqlite:///./finance.db"
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
+
 
 def get_db():
     db = SessionLocal()
--- app/main.py
+++ app/main.py
@@ -1,15 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
+from app.routers import auth, categories, transactions
 from app.db.init_db import init_db
-from app.routers import categories, auth, transactions
-
-
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(auth.router)
 app.include_router(categories.router)
 app.include_router(transactions.router)
-
 
 
 @app.on_event("startup")
